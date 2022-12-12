c4de260353e4222a25d05ba0141b3378258e5315
Sob <udreakjogos2@gmail.com>
2022-12-12 09:06:07 -0300

    Inicio do projeto

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+use Ramsey\Uuid\Type\Integer;
+
+class MarcaController extends Controller
+{
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return $marcas;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //$marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
+        return $marca;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->find($id);
+        return $marca;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        //$marca->update($request->all());
+        $marca = $this->marca->find($id);
+        $marca->update($request->all());
+        return $marca;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //$marca->delete();
+        $marca = $this->marca->find($id);
+        $marca->delete();
+        return ['msg' => 'Marca deletada com sucesso!'];
+    }
+}
