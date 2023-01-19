07089da2b10a6b7783d19606b9d91c92a5088d48
Sob <udreakjogos2@gmail.com>
2023-01-19 07:32:23 -0300

    07:30 ----- 19/01

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Bem vindo</div>
+
+                    <div class="card-body">
+                        <div class="alert alert-success" role="alert">
+                            <p>Algumas informações do back</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-header">Busca de Marca</div>
+
+                    <div class="card-body">
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputId" id-help="idhelp" texto-ajuda="Opcional. Informe o ID da marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
+                            </div>
+
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome da Marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da Marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">Relação de Marcas</div>
+
+                    <div class="card-body">
+                        <table-component></table-component>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-end">Adicionar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
